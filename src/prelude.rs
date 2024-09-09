6efabdf689a90081b4a8729551c049ed79c20165
ErisianArchitect <derekforgione@gmail.com>
2024-09-09 14:01:31 -0700

    Added more stuff to prelude.

--- src/prelude.rs
+++ src/prelude.rs
@@ -8,5 +8,17 @@ pub use crate::{
             orientation::Orientation,
         },
     },
+    io::{
+        Readable,
+        Writeable,
+    },
     math::axis::Axis,
+    util::extensions::{
+        BoolExtension,
+        OptionExtension,
+        SwapVal,
+        NumIter,
+        ResultExtension,
+    },
+    error::Result as VoxelResult,
 };
