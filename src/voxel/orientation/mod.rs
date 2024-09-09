df5245c9e78d34eb305e4a3a456eac1458af2be0
ErisianArchitect <derekforgione@gmail.com>
2024-09-08 20:12:19 -0700

    Added helper functions, and orient_table mod.

--- src/voxel/orientation/mod.rs
+++ src/voxel/orientation/mod.rs
@@ -1,3 +1,17 @@
 pub mod rotation;
 pub mod flip;
 pub mod orientation;
+mod orient_table;
+
+use flip::Flip;
+use rotation::Rotation;
+
+pub const fn pack_flip_and_rotation(flip: Flip, rotation: Rotation) -> u8 {
+    flip.0 | rotation.0 << 3
+}
+
+pub const fn unpack_flip_and_rotation(packed: u8) -> (Flip, Rotation) {
+    let flip = packed & 0b111;
+    let rotation = packed >> 3;
+    (Flip(flip), Rotation(rotation))
+}
