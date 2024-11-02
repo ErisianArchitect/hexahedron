8e3d01ad32ac3e1871da767078f52203cd971ffd
ErisianArchitect <derekforgione@gmail.com>
2024-11-02 06:01:22 -0700

    Added TagContainer

--- src/voxel/world/section.rs
+++ src/voxel/world/section.rs
@@ -1,9 +1,10 @@
-use crate::voxel::block::id::StateId;
+use crate::{collections::tagcontainer::{TagContainer, TagId}, voxel::block::id::StateId};
 
 // Holds all the block data within a 32x32x32 volume.
 pub struct Section {
     pub blocks: Option<Box<[StateId]>>,
     pub block_light: Option<Box<[u8]>>,
     pub sky_light: Option<Box<[u8]>>,
-    pub block_data_refs: Option<Box<[()]>>
+    pub block_data: Option<Box<[TagId]>>,
+    pub tag_container: TagContainer,
 }
