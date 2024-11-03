3e9d478c3c723ca7a901390cb823bdca7f978f20
ErisianArchitect <derekforgione@gmail.com>
2024-11-03 09:59:11 -0800

    Included TagSection in Section.

--- src/voxel/world/section.rs
+++ src/voxel/world/section.rs
@@ -1,10 +1,9 @@
-use crate::{collections::tagcontainer::{TagContainer, TagId}, voxel::block::id::StateId};
+use crate::{collections::tagcontainer::{TagContainer, TagId, TagSection}, voxel::block::id::StateId};
 
 // Holds all the block data within a 32x32x32 volume.
 pub struct Section {
     pub blocks: Option<Box<[StateId]>>,
     pub block_light: Option<Box<[u8]>>,
     pub sky_light: Option<Box<[u8]>>,
-    pub block_data: Option<Box<[TagId]>>,
-    pub tag_container: TagContainer,
+    pub tags: TagSection,
 }
