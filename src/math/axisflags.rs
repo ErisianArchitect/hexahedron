bc114d73aad5bae2eae4a0a803beaabe94a62dd1
ErisianArchitect <derekforgione@gmail.com>
2024-11-10 22:22:35 -0800

    Bitwise operations for AxisFlags and Axis.

--- src/math/axisflags.rs
+++ src/math/axisflags.rs
@@ -108,60 +108,136 @@ impl From<Axis> for AxisFlags {
 
 impl std::ops::BitOr<AxisFlags> for AxisFlags {
     type Output = AxisFlags;
+    
     #[inline]
     fn bitor(self, rhs: AxisFlags) -> Self::Output {
         Self(self.0 | rhs.0)
     }
 }
 
+impl std::ops::BitOr<Axis> for AxisFlags {
+    type Output = AxisFlags;
+
+    #[inline]
+    fn bitor(self, rhs: Axis) -> Self::Output {
+        self | AxisFlags::from(rhs)
+    }
+}
+
 impl std::ops::BitOrAssign<AxisFlags> for AxisFlags {
+
     #[inline]
     fn bitor_assign(&mut self, rhs: AxisFlags) {
         self.0 = self.0 | rhs.0;
     }
 }
 
+impl std::ops::BitOrAssign<Axis> for AxisFlags {
+
+    #[inline]
+    fn bitor_assign(&mut self, rhs: Axis) {
+        *self |= AxisFlags::from(rhs);
+    }
+}
+
 impl std::ops::BitAnd<AxisFlags> for AxisFlags {
     type Output = AxisFlags;
+
     #[inline]
     fn bitand(self, rhs: AxisFlags) -> Self::Output {
         Self(self.0 & rhs.0)
     }
 }
 
+impl std::ops::BitAnd<Axis> for AxisFlags {
+    type Output = AxisFlags;
+    
+    #[inline]
+    fn bitand(self, rhs: Axis) -> Self::Output {
+        self & AxisFlags::from(rhs)
+    }
+}
+
 impl std::ops::BitAndAssign<AxisFlags> for AxisFlags {
+    
     #[inline]
     fn bitand_assign(&mut self, rhs: AxisFlags) {
         self.0 = self.0 & rhs.0;
     }
 }
 
+impl std::ops::BitAndAssign<Axis> for AxisFlags {
+
+    #[inline]
+    fn bitand_assign(&mut self, rhs: Axis) {
+        *self &= AxisFlags::from(rhs);
+    }
+}
+
 impl std::ops::BitXor<AxisFlags> for AxisFlags {
     type Output = AxisFlags;
+
     #[inline]
     fn bitxor(self, rhs: AxisFlags) -> Self::Output {
         Self(self.0 ^ rhs.0)
     }
 }
 
+impl std::ops::BitXor<Axis> for AxisFlags {
+    type Output = AxisFlags;
+    
+    #[inline]
+    fn bitxor(self, rhs: Axis) -> Self::Output {
+        self ^ AxisFlags::from(rhs)
+    }
+}
+
 impl std::ops::BitXorAssign<AxisFlags> for AxisFlags {
+    
     #[inline]
     fn bitxor_assign(&mut self, rhs: AxisFlags) {
         self.0 = self.0 ^ rhs.0;
     }
 }
 
+impl std::ops::BitXorAssign<Axis> for AxisFlags {
+
+    #[inline]
+    fn bitxor_assign(&mut self, rhs: Axis) {
+        *self ^= AxisFlags::from(rhs);
+    }
+}
+
 impl std::ops::Sub<AxisFlags> for AxisFlags {
     type Output = AxisFlags;
+    
     #[inline]
     fn sub(self, rhs: AxisFlags) -> Self::Output {
         Self(self.0 & !rhs.0)
     }
 }
 
+impl std::ops::Sub<Axis> for AxisFlags {
+    type Output = AxisFlags;
+
+    #[inline]
+    fn sub(self, rhs: Axis) -> Self::Output {
+        self - AxisFlags::from(rhs)
+    }
+}
+
 impl std::ops::SubAssign<AxisFlags> for AxisFlags {
+    
     #[inline]
     fn sub_assign(&mut self, rhs: AxisFlags) {
         self.0 = self.0 & !rhs.0;
+    }
+}
+
+impl std::ops::SubAssign<Axis> for AxisFlags {
+    
+    #[inline]
+    fn sub_assign(&mut self, rhs: Axis) {
+        *self -= AxisFlags::from(rhs);
     }
 }
