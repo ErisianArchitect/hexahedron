9c430ba70d3cf27af3642d26501430f83e7664ba
ErisianArchitect <derekforgione@gmail.com>
2024-11-28 01:55:44 -0800

    Added some documentation and functionality.

--- src/io/region/region_file.rs
+++ src/io/region/region_file.rs
@@ -24,10 +24,12 @@ pub struct RegionFile {
 }
 
 impl RegionFile {
+    #[inline]
     pub fn get_timestamp<C: Into<RegionCoord>>(&self, coord: C) -> Timestamp {
         self.header.timestamps[coord.into()]
     }
 
+    /// Opens an existing region file.
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Self> {
         let mut file_handle = File::options()
             .read(true)
@@ -70,6 +72,7 @@ impl RegionFile {
         })
     }
 
+    /// Create a new region file or overwrite it if it already exists.
     pub fn create<P: AsRef<Path>>(path: P) -> Result<Self> {
         let path = path.as_ref();
         let parent = path.parent().ok_or(Error::ParentNotFound)?;
@@ -101,6 +104,7 @@ impl RegionFile {
         }
     }
 
+    /// Read from a sub-file at the coordinate in the region file using a callback that takes the reader as its only argument.
     pub fn read<'a, C: Into<RegionCoord>, R, F: FnOnce(&mut GzDecoder<Take<BufReader<&'a mut File>>>) -> Result<R>>(&'a mut self, coord: C, read: F) -> Result<R> {
         let coord: RegionCoord = coord.into();
         let sector = self.header.offsets[coord];
@@ -114,6 +118,8 @@ impl RegionFile {
         read(&mut decoder)
     }
 
+    /// Read a value from the sub-file at the coordinate within the region file.
+    #[inline]
     pub fn read_value<C: Into<RegionCoord>, T: Readable>(&mut self, coord: C) -> Result<T> {
         fn read_inner<'a, T: Readable>(reader: &mut GzDecoder<Take<BufReader<&'a mut File>>>) -> Result<T> {
             T::read_from(reader)
@@ -121,15 +127,15 @@ impl RegionFile {
         self.read(coord, read_inner)
     }
 
-    /// If nothing is written to the writer, then it will delete the entry in the
-    /// region file.
+    /// Write to a sub-file at the coordinate within the region file using a callback that takes the writer as its only argument.  
+    /// If nothing is written to the writer, then it will delete the entry in the region file.
     pub fn write<C: Into<RegionCoord>, F: FnOnce(&mut GzEncoder<&mut Cursor<Vec<u8>>>) -> Result<()>>(&mut self, coord: C, write: F) -> Result<()> {
         let coord: RegionCoord = coord.into();
         self.write_buffer.seek(SeekFrom::Start(0))?;
         self.write_buffer.get_mut().clear();
         let mut encoder = GzEncoder::new(&mut self.write_buffer, Compression::fast());
-        // write to encoder in the write buffer
         {
+            // write to encoder using the supplied callback.
             write(&mut encoder)?;
         }
         // Check if length is zero, which means nothing was written.
@@ -163,6 +169,8 @@ impl RegionFile {
         Ok(())
     }
 
+    /// Write a value to the sub-file at the coordinate in the region file.
+    #[inline]
     pub fn write_value<C: Into<RegionCoord>, T: Writeable>(&mut self, coord: C, value: &T) -> Result<()> {
         self.write(coord, move |writer| {
             value.write_to(writer)?;
@@ -170,13 +178,35 @@ impl RegionFile {
         })
     }
 
-    pub fn write_timestamped<C: Into<RegionCoord>, Ts: Into<Timestamp>, F: FnMut(&mut GzEncoder<&mut Cursor<Vec<u8>>>) -> Result<()>>(&mut self, coord: C, timestamp: Ts, write: F) -> Result<()> {
+    /// Write a timestamped value to the sub-file at the coordinate in the region file using a callback that takes the writer as its only argument.
+    #[inline]
+    pub fn write_timestamped<C: Into<RegionCoord>, Ts: Into<Timestamp>, F: FnOnce(&mut GzEncoder<&mut Cursor<Vec<u8>>>) -> Result<()>>(&mut self, coord: C, timestamp: Ts, write: F) -> Result<()> {
         let coord: RegionCoord = coord.into();
         self.write(coord, write)?;
         self.write_timestamp(coord, timestamp)
     }
 
-    pub fn write_timestamp<C: Into<RegionCoord>, Ts: Into<Timestamp>>(&mut self, coord: C, timestamp: Ts) -> Result<()> {
+    /// Write a value to the sub-file at the coordinate in the region file.
+    #[inline]
+    pub fn write_value_timestamped<C: Into<RegionCoord>, T: Writeable, Ts: Into<Timestamp>>(&mut self, coord: C, value: &T, timestamp: Ts) -> Result<()> {
+        self.write_timestamped(coord.into() as RegionCoord, timestamp.into() as Timestamp, move |writer| {
+            value.write_to(writer)?;
+            Ok(())
+        })
+    }
+
+    #[inline]
+    pub fn write_with_utc_now<C: Into<RegionCoord>, F: FnOnce(&mut GzEncoder<&mut Cursor<Vec<u8>>>) -> Result<()>>(&mut self, coord: C, write: F) -> Result<()> {
+        self.write_timestamped(coord.into() as RegionCoord, Timestamp::utc_now(), write)
+    }
+
+    #[inline]
+    pub fn write_value_with_utc_now<C: Into<RegionCoord>, T: Writeable>(&mut self, coord: C, value: &T) -> Result<()> {
+        self.write_value_timestamped(coord.into() as RegionCoord, value, Timestamp::utc_now())
+    }
+
+    /// Write a timestamp to the header table.
+    fn write_timestamp<C: Into<RegionCoord>, Ts: Into<Timestamp>>(&mut self, coord: C, timestamp: Ts) -> Result<()> {
         let coord: RegionCoord = coord.into();
         let timestamp: Timestamp = timestamp.into();
         self.header.timestamps[coord] = timestamp;
@@ -187,6 +217,7 @@ impl RegionFile {
         Ok(())
     }
 
+    /// Delete an entry from the region file.
     pub fn delete_data<C: Into<RegionCoord>>(&mut self, coord: C) -> Result<()> {
         let coord: RegionCoord = coord.into();
         let sector = self.header.offsets[coord];
@@ -204,4 +235,11 @@ impl RegionFile {
         writer.flush()?;
         Ok(())
     }
+
+    // /// Defragments the region file.
+    // pub fn defragment(&mut self) -> Result<()> {
+    //     // This is a very complex operation, so I'm going to put off
+    //     // using it.
+    //     unimplemented!()
+    // }
 }
