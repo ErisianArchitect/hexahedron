1c99563cd6d9af1201403a01c13d1c9b0e9eef59
ErisianArchitect <derekforgione@gmail.com>
2024-12-02 14:46:53 -0800

    Commented out section_index_impls

--- src/voxel/world/section/mod.rs
+++ src/voxel/world/section/mod.rs
@@ -9,7 +9,7 @@ mod section;
 use glam::IVec3;
 pub use section::*;
 
-use crate::{for_each_int_type, math::index3};
+use crate::math::index3;
 
 pub trait SectionIndex<const W: i32>: Copy {
     fn section_index(self) -> usize;
@@ -36,20 +36,23 @@ impl<const W: i32> SectionIndex<W> for (i32, i32, i32) {
     }
 }
 
-macro_rules! section_index_impls {
-    ($type:ty) => {
-        impl<const W: i32> SectionIndex<W> for $type {
-            fn section_index(self) -> usize {
-                self as usize
-            }
-        }
-    };
-}
+// If I blanket implement SectionIndex for all unsigned integer types,
+// type resolution won't know which one to pick if you don't specify type.
+// It's best if I stick to implementing SectionIndex for only usize.
+// macro_rules! section_index_impls {
+//     ($type:ty) => {
+//         impl<const W: i32> SectionIndex<W> for $type {
+//             fn section_index(self) -> usize {
+//                 self as usize
+//             }
+//         }
+//     };
+// }
+// 
+// for_each_int_type!(unsigned; section_index_impls);
 
 impl<const W: i32> SectionIndex<W> for usize {
     fn section_index(self) -> usize {
         self
     }
 }
-
-// for_each_int_type!(unsigned; section_index_impls);
