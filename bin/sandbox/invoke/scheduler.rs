00aa7a47afeee7b4bab2eaf238b5283496b98a54
ErisianArchitect <derekforgione@gmail.com>
2024-12-15 12:39:11 -0800

    Removed PhantomData and changed Marker type.

--- bin/sandbox/invoke/scheduler.rs
+++ bin/sandbox/invoke/scheduler.rs
@@ -8,123 +8,123 @@ use super::callback::{Callback, *};
 
 #[derive(Default)]
 pub struct Scheduler<Ctx: SchedulerContext> {
-    phantom: PhantomData<(Ctx)>,
+    // phantom: PhantomData<(Ctx)>,
     pub(crate) schedule_heap: BinaryHeap<TimeKey<Ctx>>,
 }
 
 impl<Ctx: SchedulerContext> Scheduler<Ctx> {
     pub fn new() -> Self {
         Self {
-            phantom: PhantomData,
+            // phantom: PhantomData,
             schedule_heap: BinaryHeap::new(),
         }
     }
 
     #[inline]
-    pub fn at<I, F>(&mut self, time: Instant, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn at<M, F>(&mut self, time: Instant, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.schedule_heap.push(TimeKey::new(time, Box::new(callback.into_callback())));
     }
 
     #[inline]
-    pub fn after<I, F>(&mut self, duration: Duration, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after<M, F>(&mut self, duration: Duration, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.at(Instant::now() + duration, callback);
     }
 
     #[inline]
-    pub fn now<I, F>(&mut self, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn now<M, F>(&mut self, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.at(Instant::now(), callback);
     }
 
     #[inline]
-    pub fn after_micros<I, F>(&mut self, micros: u64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_micros<M, F>(&mut self, micros: u64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_micros(micros), callback);
     }
 
     #[inline]
-    pub fn after_millis<I, F>(&mut self, millis: u64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_millis<M, F>(&mut self, millis: u64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_millis(millis), callback);
     }
 
     #[inline]
-    pub fn after_nanos<I, F>(&mut self, nanos: u64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_nanos<M, F>(&mut self, nanos: u64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_nanos(nanos), callback);
     }
 
     #[inline]
-    pub fn after_secs<I, F>(&mut self, secs: u64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_secs<M, F>(&mut self, secs: u64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs(secs), callback)
     }
 
     #[inline]
-    pub fn after_secs_f32<I, F>(&mut self, secs: f32, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_secs_f32<M, F>(&mut self, secs: f32, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs_f32(secs), callback);
     }
 
     #[inline]
-    pub fn after_secs_f64<I, F>(&mut self, secs: f64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_secs_f64<M, F>(&mut self, secs: f64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs_f64(secs), callback);
     }
 
     #[inline]
-    pub fn after_mins<I, F>(&mut self, mins: u64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_mins<M, F>(&mut self, mins: u64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs(mins * 60), callback);
     }
 
     #[inline]
-    pub fn after_mins_f32<I, F>(&mut self, mins: f32, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_mins_f32<M, F>(&mut self, mins: f32, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs_f32(mins * 60.0), callback);
     }
 
     #[inline]
-    pub fn after_mins_f64<I, F>(&mut self, mins: f64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_mins_f64<M, F>(&mut self, mins: f64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs_f64(mins * 60.0), callback);
     }
 
     #[inline]
-    pub fn after_hours<I, F>(&mut self, hours: u64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_hours<M, F>(&mut self, hours: u64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs(hours * 3600), callback);
     }
 
     #[inline]
-    pub fn after_hours_f32<I, F>(&mut self, hours: f32, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_hours_f32<M, F>(&mut self, hours: f32, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs_f32(hours * 3600.0), callback);
     }
 
     #[inline]
-    pub fn after_hours_f64<I, F>(&mut self, hours: f64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_hours_f64<M, F>(&mut self, hours: f64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs_f64(hours * 3600.0), callback);
     }
 
     #[inline]
-    pub fn after_days<I, F>(&mut self, days: u64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_days<M, F>(&mut self, days: u64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs(days * 86400), callback);
     }
 
     #[inline]
-    pub fn after_days_f32<I, F>(&mut self, days: f32, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_days_f32<M, F>(&mut self, days: f32, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs_f32(days * 86400.0), callback);
     }
 
     #[inline]
-    pub fn after_days_f64<I, F>(&mut self, days: f64, callback: F)
-    where F: IntoCallback<Ctx, I> {
+    pub fn after_days_f64<M, F>(&mut self, days: f64, callback: F)
+    where F: IntoCallback<Ctx, M> {
         self.after(Duration::from_secs_f64(days * 86400.0), callback);
     }
 
