8e37cabeabf5c9108d7893f48fcaff5e7f572ae0
ErisianArchitect <derekforgione@gmail.com>
2025-01-13 20:05:02 -0800

    Added private module which contains Sealed trait and seal macro.

---
+++ src/private.rs
@@ -0,0 +1,27 @@
+/// When a trait is constrained to [Sealed], allows for that trait to be sealed from being implemented externally.
+pub trait Sealed<T> {}
+
+#[macro_export]
+macro_rules! seal {
+    ($marker:ty; $($type:ty),+$(,)?) => {
+        $(
+            impl $crate::private::Sealed<$marker> for $type {}
+        )+
+    };
+    ($($type:ty),+$(,)?) => {
+        $(
+            impl $crate::private::Sealed<()> for $type {}
+        )+
+    };
+    ($marker:ty; where: $($trait:path),+$(,)?) => {
+        impl<T> $crate::private::Sealed<$marker> for T
+        where T: $($trait+)+ {}
+    };
+    (where: $($trait:path),+$(,)?) => {
+        impl<T> $crate::private::Sealed<()> for T
+        where T: $($trait+)+ {}
+    };
+}
+
+#[allow(unused)]
+pub use crate::seal;
