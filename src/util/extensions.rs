892ba4de4ef9854029fc6c73d589c95a37e067bc
ErisianArchitect <derekforgione@gmail.com>
2025-02-13 21:54:59 -0800

    A bunch of changes that I forgot to track.

--- src/util/extensions.rs
+++ src/util/extensions.rs
@@ -1,12 +1,68 @@
 use std::ops::{Range, RangeInclusive};
 use crate::macros::for_each_int_type;
+use crate::private::*;
+
+pub trait ToSome: Sized {
+    fn some(self) -> Option<Self>;
+}
+
+impl<T: Sized> ToSome for T {
+    #[inline]
+    fn some(self) -> Option<Self> {
+        Some(self)
+    }
+}
+
+pub trait ArrayOfOne: Sized {
+    fn array_of_one(self) -> [Self; 1];
+}
+
+impl<T: Sized> ArrayOfOne for T {
+    fn array_of_one(self) -> [Self; 1] {
+        [self]
+    }
+}
+
+pub trait AsSliceOfOne: Sized {
+    fn as_slice_of_one(&self) -> &[Self];
+}
+
+impl<T: Sized> AsSliceOfOne for T {
+    fn as_slice_of_one(&self) -> &[Self] {
+        unsafe {
+            std::slice::from_raw_parts(self, 1)
+        }
+    }
+}
+
+pub trait AsSliceOfOneMut: Sized {
+    fn as_slice_of_one_mut(&mut self) -> &mut [Self];
+}
+
+impl<T: Sized> AsSliceOfOneMut for T {
+    fn as_slice_of_one_mut(&mut self) -> &mut [Self] {
+        unsafe {
+            std::slice::from_raw_parts_mut(self, 1)
+        }
+    }
+}
+
+pub trait TupleOfOne {
+    fn tuple_of_one(self) -> (Self,);
+}
+
+impl<T: Sized> TupleOfOne for T {
+    fn tuple_of_one(self) -> (Self,) {
+        (self,)
+    }
+}
 
 /// [Replace] allows for in-place replacement of values.
-pub trait Replace {
+pub trait Replace: Sized {
     fn replace(&mut self, src: Self) -> Self;
 }
 
-impl<T> Replace for T {
+impl<T: Sized> Replace for T {
     /// Replaces `self` with `src`.
     #[inline(always)]
     fn replace(&mut self, src: Self) -> Self {
@@ -14,30 +70,25 @@ impl<T> Replace for T {
     }
 }
 
-/// [TakeReplace] allows for in-place replacement of values using a transformer function.
-pub trait TakeReplace: Sized {
-    fn take_replace<F: FnOnce(Self) -> Self>(&mut self, replace: F);
+/// [ReplaceWith] allows for in-place replacement of values using a transformer function.
+pub trait ReplaceWith: Sized {
+    fn replace_with<F: FnOnce(Self) -> Self>(&mut self, replace: F);
 }
 
-impl<T: Sized> TakeReplace for T {
+impl<T: Sized> ReplaceWith for T {
     /// Takes the value and replaces it using a function that takes the value as input and returns the new value.
     #[inline(always)]
-    fn take_replace<F: FnOnce(Self) -> Self>(&mut self, replace: F) {
+    fn replace_with<F: FnOnce(Self) -> Self>(&mut self, replace: F) {
         unsafe {
             std::ptr::write(self, replace(std::ptr::read(self)));
         }
     }
 }
 
-/// Private module contained the [Sealed] trait.
-mod private {
-    pub trait Sealed<T> {}
-}
-
-impl<T> private::Sealed<Option<T>> for Option<T> {}
+seal!(marker = Option<T>; <T> for Option<T>);
 
 /// An extension to the [Option] type.
-pub trait OptionExtension<T>: private::Sealed<Option<T>> {
+pub trait OptionExtension<T>: Sealed<Option<T>> {
     fn then<F: FnOnce(T)>(self, then: F);
     fn drop(&mut self);
 }
@@ -58,51 +109,51 @@ impl<T> OptionExtension<T> for Option<T> {
     }
 }
 
-impl private::Sealed<bool> for bool {}
+seal!(marker = bool; bool);
 
 /// An extension to the [bool] type.
-pub trait BoolExtension: private::Sealed<bool> {
-    fn select<T>(self, _true: T, _false: T) -> T;
-    fn select_fn<T, TF: FnOnce() -> T, FF: FnOnce() -> T>(self, _true: TF, _false: FF) -> T;
-    fn select_unary<T, V, TF: FnOnce(V) -> T, FF: FnOnce(V) -> T>(self, value: V, _true: TF, _false: FF) -> T;
+pub trait BoolExtension: Sealed<bool> {
+    fn select<T>(self, true_: T, false_: T) -> T;
+    fn select_fn<T, TF: FnOnce() -> T, FF: FnOnce() -> T>(self, true_: TF, false_: FF) -> T;
+    fn select_unary<T, V, TF: FnOnce(V) -> T, FF: FnOnce(V) -> T>(self, value: V, true_: TF, false_: FF) -> T;
     fn mark(&mut self) -> bool;
     fn mark_if(&mut self, condition: bool) -> bool;
     fn unmark(&mut self) -> bool;
     fn unmark_if(&mut self, condition: bool) -> bool;
     fn toggle(&mut self) -> Self;
     fn toggle_if(&mut self, condition: bool) -> Self;
-    fn if_<F: FnOnce()>(self, _if: F);
-    fn if_not<F: FnOnce()>(self, _not: F);
-    fn if_else<R, If: FnOnce() -> R, Else: FnOnce() -> R>(self, _if: If, _else: Else) -> R;
+    fn if_<F: FnOnce()>(self, if_: F);
+    fn if_not<F: FnOnce()>(self, not_: F);
+    fn if_else<R, If: FnOnce() -> R, Else: FnOnce() -> R>(self, if_: If, else_: Else) -> R;
 }
 
 impl BoolExtension for bool {
     /// Choose a truth value or a false value.
     #[inline]
-    fn select<T>(self, _true: T, _false: T) -> T {
+    fn select<T>(self, true_: T, false_: T) -> T {
         if self {
-            _true
+            true_
         } else {
-            _false
+            false_
         }
     }
 
     /// Execute and return the value of _false or _true depending if self is false or true.
     #[inline]
-    fn select_fn<T, TF: FnOnce() -> T, FF: FnOnce() -> T>(self, _true: TF, _false: FF) -> T {
+    fn select_fn<T, TF: FnOnce() -> T, FF: FnOnce() -> T>(self, true_: TF, false_: FF) -> T {
         if self {
-            _true()
+            true_()
         } else {
-            _false()
+            false_()
         }
     }
 
     #[inline]
-    fn select_unary<T, V, TF: FnOnce(V) -> T, FF: FnOnce(V) -> T>(self, value: V, _true: TF, _false: FF) -> T {
+    fn select_unary<T, V, TF: FnOnce(V) -> T, FF: FnOnce(V) -> T>(self, value: V, true_: TF, false_: FF) -> T {
         if self {
-            _true(value)
+            true_(value)
         } else {
-            _false(value)
+            false_(value)
         }
     }
 
@@ -116,7 +167,9 @@ impl BoolExtension for bool {
     #[inline]
     fn mark_if(&mut self, condition: bool) -> bool {
         if condition {
-            !std::mem::replace(self, true)
+            let changed = !*self;
+            *self = true;
+            changed
         } else {
             false
         }
@@ -125,14 +178,18 @@ impl BoolExtension for bool {
     /// Sets value to false and returns true if it was previously true.
     #[inline]
     fn unmark(&mut self) -> bool {
-        std::mem::replace(self, false)
+        let changed = *self;
+        *self = false;
+        changed
     }
 
     /// If the condition is met, sets the value to false and returns true if the value was changed.
     #[inline]
     fn unmark_if(&mut self, condition: bool) -> bool {
         if condition {
-            std::mem::replace(self, false)
+            let changed = *self;
+            *self = false;
+            changed
         } else {
             false
         }
@@ -156,34 +213,50 @@ impl BoolExtension for bool {
 
     /// `if self { _if() }`
     #[inline]
-    fn if_<F: FnOnce()>(self, _if: F) {
-        if self { _if() }
+    fn if_<F: FnOnce()>(self, if_: F) {
+        if self { if_() }
     }
     
     /// `if !self { _not() }`
     #[inline]
-    fn if_not<F: FnOnce()>(self, _not: F) {
-        if !self { _not() }
+    fn if_not<F: FnOnce()>(self, not_: F) {
+        if !self { not_() }
     }
 
     /// Like `if-else`, but with closures!
     #[inline]
-    fn if_else<R, If: FnOnce() -> R, Else: FnOnce() -> R>(self, _if: If, _else: Else) -> R {
-        self.select_fn(_if, _else)
+    fn if_else<R, If: FnOnce() -> R, Else: FnOnce() -> R>(self, if_: If, else_: Else) -> R {
+        self.select_fn(if_, else_)
     }
 }
 
 /// An extension to integer types for iteration.
-pub trait NumIter: Sized + Copy + private::Sealed<()> {
+pub trait NumIter: Sized + Copy + Sealed<()> {
     fn iter(self) -> Range<Self>;
     fn iter_inclusive(self) -> RangeInclusive<Self>;
     fn iter_to(self, end: Self) -> Range<Self>;
     fn iter_to_inclusive(self, end: Self) -> RangeInclusive<Self>;
 }
 
-macro_rules! num_iter_impls {
+/// An extension for integer types for incrementation.
+pub trait Increment: Sealed<()> {
+    /// Increment and return the result of incrementation.
+    fn increment(&mut self) -> Self;
+    /// Increment and return the value prior to incrementation.
+    fn post_increment(&mut self) -> Self;
+}
+
+/// An extension for integer types for decrementation.
+pub trait Decrement: Sealed<()> {
+    /// Decrement and return the result of decrementation.
+    fn decrement(&mut self) -> Self;
+    /// Decrement and return the value prior to decrementation.
+    fn post_decrement(&mut self) -> Self;
+}
+
+macro_rules! num_impls {
     ($type:ty) => {
-        impl private::Sealed<()> for $type {}
+        seal!($type);
         impl NumIter for $type {
             #[doc = "Returns a [Range] from [0] to [self]."]
             #[inline]
@@ -209,29 +282,7 @@ macro_rules! num_iter_impls {
                 self..=end
             }
         }
-    };
-}
 
-for_each_int_type!(num_iter_impls);
-
-/// An extension for integer types for incrementation.
-pub trait Increment: private::Sealed<()> {
-    /// Increment and return the result of incrementation.
-    fn increment(&mut self) -> Self;
-    /// Increment and return the value prior to incrementation.
-    fn post_increment(&mut self) -> Self;
-}
-
-/// An extension for integer types for decrementation.
-pub trait Decrement: private::Sealed<()> {
-    /// Decrement and return the result of decrementation.
-    fn decrement(&mut self) -> Self;
-    /// Decrement and return the value prior to decrementation.
-    fn post_decrement(&mut self) -> Self;
-}
-
-macro_rules! inc_dec_impls {
-    ($type:ty) => {
         impl Increment for $type {
             #[doc = "Increment [self] by [1] and return the result."]
             #[inline]
@@ -268,19 +319,19 @@ macro_rules! inc_dec_impls {
     };
 }
 
-for_each_int_type!(inc_dec_impls);
+for_each_int_type!(num_impls);
 
 /// An extension for [Result].
-pub trait ResultExtension: private::Sealed<Result<(), ()>> {
+pub trait ResultExtension: Sealed<Result<(), ()>> {
     type Ok;
     type Error;
     fn handle_err<F: FnOnce(Self::Error)>(self, f: F);
     fn try_fn<F: FnOnce() -> Self>(f: F) -> Self;
 }
 
-impl<T, E> private::Sealed<std::result::Result<(), ()>> for std::result::Result<T, E> {}
+seal!(marker = Result<(), ()>; <T, E> for Result<T, E>);
 
-impl<T, E> ResultExtension for std::result::Result<T, E> {
+impl<T, E> ResultExtension for Result<T, E> {
     type Ok = T;
     type Error = E;
     /// For when you want to ignore the return value of a result but you also want to handle the error if there is one.
@@ -370,12 +421,12 @@ mod tests {
 
         let mut option = Options::Apple(String::from("hello, world"));
         assert_eq!(option, Options::Apple(String::from("hello, world")));
-        option.take_replace(|option| match option {
+        option.replace_with(|option| match option {
             Options::Apple(text) => Options::Banana(text, 0),
             Options::Banana(text, count) => Options::Banana(text, count + 1),
         });
         assert_eq!(option, Options::Banana(String::from("hello, world"), 0));
-        option.take_replace(|option| match option {
+        option.replace_with(|option| match option {
             Options::Apple(text) => Options::Banana(text, 0),
             Options::Banana(text, count) => Options::Banana(text, count + 1),
         });
