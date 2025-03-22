655cce07dc36043259acf44ef89e66b2d2591265
ErisianArchitect <derekforgione@gmail.com>
2025-03-22 03:30:51 -0700

    Created measure_time macro.

--- hexcore/src/macros.rs
+++ hexcore/src/macros.rs
@@ -14,3 +14,29 @@ macro_rules! pipeline {
 
 
 pub use crate::pipeline;
+
+pub struct MeasureTimeResult<T> {
+    pub start_time: std::time::Instant,
+    pub result: T,
+    pub duration: std::time::Duration,
+}
+
+#[macro_export]
+macro_rules! measure_time {
+    ($($token:tt)*) => {
+        {
+            let start_time = std::time::Instant::now();
+            let result = {
+                $($token)*
+            };
+            let duration = start_time.elapsed();
+            $crate::macros::MeasureTimeResult {
+                start_time,
+                result,
+                duration,
+            }
+        }
+    };
+}
+
+pub use crate::measure_time;
