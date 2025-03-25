82401661f8107d8c98626432fec053a805de43aa
ErisianArchitect <derekforgione@gmail.com>
2025-03-25 06:39:12 -0700

    Doin some stuff.

--- src/engine/scene.rs
+++ src/engine/scene.rs
@@ -8,14 +8,18 @@
 - Render
 */
 
-use std::rc::Rc;
-use std::cell::RefCell;
+use std::any::Any;
+use std::{rc::Rc, sync::atomic::AtomicBool};
+use std::cell::{RefCell, UnsafeCell};
 
 use super::{Engine, EventPropagation, frames::frame_info::FrameInfo};
 
 
 #[allow(unused)]
-pub trait Scene {
+pub trait Scene: Any {
+
+    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&mut self) -> &mut dyn Any;
 
     fn load(
         &mut self,
@@ -25,6 +29,12 @@ pub trait Scene {
     fn unload(
         &mut self,
         engine: &Engine,
+    ) {}
+
+    fn on_active_changed(
+        &mut self,
+        engine: &Engine,
+        active: bool,
     ) {}
 
     fn resize(
@@ -135,9 +145,36 @@ pub trait Scene {
     ) {}
 }
 
+struct SceneCell {
+    scene: Box<UnsafeCell<dyn Scene + 'static>>,
+    active: AtomicBool,
+}
+
+impl SceneCell {
+    #[inline]
+    pub fn new<S: Scene + 'static>(scene: S, active: bool) -> Self {
+        Self {
+            scene: Box::new(UnsafeCell::new(scene)),
+            active: AtomicBool::new(active),
+        }
+    }
+
+    #[inline]
+    fn get_scene_mut(&self) -> &mut dyn Scene {
+        unsafe {
+            self.scene.get().as_mut().unwrap()
+        }
+    }
+
+    #[inline]
+    fn set_active(&self, active: bool) {
+        self.active.store(active, std::sync::atomic::Ordering::Relaxed);
+    }
+}
+
 #[derive(Clone)]
 pub struct SharedScene {
-    scene: Rc<RefCell<dyn Scene + 'static>>,
+    cell: Rc<SceneCell>,
 }
 
 impl<T: Scene + 'static> From<T> for SharedScene {
@@ -147,141 +184,216 @@ impl<T: Scene + 'static> From<T> for SharedScene {
 }
 
 impl SharedScene {
-    pub fn new<S: Scene + 'static>(scene: S) -> Self {
+    #[inline]
+    pub fn new<S: Scene + 'static + Sized>(scene: S) -> Self {
         Self {
-            scene: Rc::new(RefCell::new(scene)),
+            cell: Rc::new(SceneCell::new(scene, true)),
         }
     }
 
+    #[inline]
+    pub fn set_active(&self, engine: &Engine, active: bool) {
+        self.cell.set_active(active);
+        self.on_active_changed(engine, active);
+    }
+
+    #[inline]
+    fn on_active_changed(&self, engine: &Engine, active: bool) {
+        self.cell.get_scene_mut().on_active_changed(engine, active);
+    }
+
+    #[inline]
     pub fn load(
         &self,
         engine: &Engine,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.load(engine);
+        self.cell.get_scene_mut().load(engine);
     }
 
+    #[inline]
     pub fn unload(
         &self,
         engine: &Engine,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.unload(engine);
+        self.cell.get_scene_mut().unload(engine);
     }
 
+    #[inline]
     pub fn resize(
         &self,
         engine: &Engine,
         new_size: winit::dpi::PhysicalSize<u32>,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.resize(engine, new_size);
+        self.cell.get_scene_mut().resize(engine, new_size);
     }
 
+    #[inline]
     pub fn focus_changed(
         &self,
         engine: &Engine,
         focused: bool,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.focus_changed(engine, focused);
+        self.cell.get_scene_mut().focus_changed(engine, focused);
     }
 
+    #[inline]
     pub fn close_requested(
         &self,
         engine: &Engine,
     ) -> bool {
-        let mut scene = self.scene.borrow_mut();
-        scene.close_requested(engine)
+        self.cell.get_scene_mut().close_requested(engine)
     }
 
+    #[inline]
     pub fn process_event(
         &self,
         engine: &Engine,
         event: &winit::event::Event<()>,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.process_event(engine, event);
+        self.cell.get_scene_mut().process_event(engine, event);
     }
 
+    #[inline]
     pub fn process_window_event(
         &self,
         engine: &Engine,
         event: &winit::event::WindowEvent,
     ) -> EventPropagation {
-        let mut scene = self.scene.borrow_mut();
-        scene.process_window_event(engine, event)
+        self.cell.get_scene_mut().process_window_event(engine, event)
     }
 
+    #[inline]
     pub fn begin_frame(
         &self,
         engine: &Engine,
         frame_index: u64,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.begin_frame(engine, frame_index);
+        self.cell.get_scene_mut().begin_frame(engine, frame_index);
     }
 
+    #[inline]
     pub fn end_frame(
         &self,
         engine: &Engine,
         frame_index: u64,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.end_frame(engine, frame_index);
+        self.cell.get_scene_mut().end_frame(engine, frame_index);
     }
 
+    #[inline]
     pub fn begin_update(
         &self,
         engine: &Engine,
         frame: &FrameInfo,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.begin_update(engine, frame);
+        self.cell.get_scene_mut().begin_update(engine, frame);
     }
 
+    #[inline]
     pub fn update(
         &self,
         engine: &Engine,
         frame: &FrameInfo,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.update(engine, frame);
+        self.cell.get_scene_mut().update(engine, frame);
     }
 
+    #[inline]
     pub fn end_update(
         &self,
         engine: &Engine,
         frame: &FrameInfo,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.end_update(engine, frame);
+        self.cell.get_scene_mut().end_update(engine, frame);
     }
 
+    #[inline]
     pub fn begin_render(
         &self,
         engine: &Engine,
         frame: &FrameInfo,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.begin_render(engine, frame);
+        self.cell.get_scene_mut().begin_render(engine, frame);
     }
 
+    #[inline]
     pub fn render(
         &self,
         engine: &Engine,
         frame: &FrameInfo,
     ) -> Result<(), wgpu::SurfaceError> {
-        let mut scene = self.scene.borrow_mut();
-        scene.render(engine, frame)
+        self.cell.get_scene_mut().render(engine, frame)
     }
 
+    #[inline]
     pub fn end_render(
         &self,
         engine: &Engine,
         frame: &FrameInfo,
     ) {
-        let mut scene = self.scene.borrow_mut();
-        scene.end_render(engine, frame);
+        self.cell.get_scene_mut().end_render(engine, frame);
+    }
+
+    #[inline]
+    pub fn cast<S: Scene>(&self) -> Option<&S> {
+        unsafe {
+            let scene_obj: &dyn Scene = &*self.cell.scene.get();
+            scene_obj.as_any().downcast_ref()
+        }
+    }
+
+    #[inline]
+    pub unsafe fn cast_mut<S: Scene>(&self) -> Option<&mut S> {
+        unsafe {
+            let scene_obj: &mut dyn Scene = &mut *self.cell.scene.get();
+            scene_obj.as_any_mut().downcast_mut()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::mem::MaybeUninit;
+
+    use super::*;
+    #[test]
+    fn scene_test() {
+        struct TestScene {
+            value: String,
+        }
+        impl Scene for TestScene {
+            fn as_any(&self) -> &dyn Any {
+                self
+            }
+
+            fn as_any_mut(&mut self) -> &mut dyn Any {
+                self
+            }
+
+            fn load(
+                    &mut self,
+                    engine: &Engine,
+                ) {
+                self.value = "hello, world".to_owned();
+            }
+
+            fn unload(
+                    &mut self,
+                    engine: &Engine,
+                ) {
+                println!("{}", self.value);
+            }
+        }
+        let scene = SharedScene::new(TestScene { value: String::new() });
+        unsafe {
+            let engine = MaybeUninit::uninit().assume_init();
+            scene.load(&engine);
+            scene.unload(&engine);
+            unsafe {
+                scene.cast_mut::<TestScene>().unwrap().value = "this is a test.".to_owned();
+            }
+            scene.unload(&engine);
+            std::mem::forget(engine);
+        }
     }
 }
